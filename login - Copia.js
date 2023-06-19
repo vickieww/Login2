e75acde92f6dff6a0bf21783af320ad0894e1665
Vitoria <gabrieli.vitoria@alunoescolassociais.org.br>
2023-06-19 12:05:30 -0300

    first commit

---
+++ login - Copia.js
@@ -0,0 +1,17 @@
+//Código de validação do formulário de login
+
+
+
+function Verificar(){
+ let email = document.getElementById('loginEmail').value;
+let number = document.getElementById('senha').value;
+ if(!email || !number){
+ alert("Campo de preenchimento obrigatório. Favor preencher");
+}
+
+}
+
+
+
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//kkkkklkklkkkkkkkkkkhahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha
